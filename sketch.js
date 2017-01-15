34796bce71b6f0d35091987190db8a529644b47f
monicabocelli <monica.bocelli93@gmail.com>
2017-01-15 13:23:37 +0100

    uploaded files

---
+++ sketch.js
@@ -0,0 +1,98 @@
+var dots = [];
+
+var value = 0;  //starting value of earthquake
+    
+
+function setup(){
+    createCanvas(500, 800);
+    
+    //create objects
+    for (var i = 0; i < value*5; i++){
+        dots.push(new QuakeDots());
+    }
+    
+   // image(img, 0, 0);
+}
+
+function draw(){
+    background(204);
+    
+    if (value = 0){
+        
+    textFont('Inconsolata');
+    textSize(30);
+    textAlign(CENTER);
+    textStyle(NORMAL);
+    text("SHAKE YOUR DEVICE", width/2, height/2); 
+    
+    } else if (value > 0){
+        
+    deviceShaken();
+    
+    //draw dots and given methods (actions)
+      noStroke();
+      fill(0);
+      for (var i = 0; i < value*5; i++){
+        dots[i].move();
+        dots[i]. display();
+        
+      }
+        textSize(20);
+        text('SEE RESULTS', width/2, height/1.1);
+    };
+    
+}
+   
+
+function deviceShaken(){
+    
+    QuakeDots();    
+    
+    value = accelerationX * accelerationY * accelerationZ;
+    magnitude = map(value, 0, 1000, 0, 10);
+    
+    //CREATE THE ELLIPSE AREA
+    var x = width/2;
+    var y = height/2;
+    var r = value; 
+    
+    noFill();
+    stroke(0);
+    strokeWeight(1);
+    ellipse (x, y, r, r);
+    
+    //magnitude indication
+    textFont('Inconsolata');
+    textSize(12);
+    textAlign(CENTER);
+    textStyle(NORMAL);
+    text("Magnitude", width - width/6, height/3.5);
+    textSize(15);
+    textAlign(CENTER);
+    textStyle(BOLD); 
+    
+    text(magnitude, width - width/6, height/2); //COME SOPRA IN DRAW()
+}
+
+
+function QuakeDots(){
+    
+    var d = dist(width/2,height/2,width/2+value/2,height/2);
+    
+    this.xdot = random(width/2 - d, width/2 + d); //according to ellipse area
+    this.ydot = random(height/2 - d, height/2 + d); //according to ellipse area
+    this.diameter = 4;
+    this.speed = 1.5; //magnitude
+
+
+    this.move = function(){
+      this.xdot += random(-this.speed,this.speed);
+      this.ydot += random(-this.speed,this.speed);
+    }
+
+    this.display = function(){
+      ellipse(this.xdot, this.ydot, this.diameter, this.diameter);
+    };
+
+    
+}

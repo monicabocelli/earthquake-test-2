410d0fc1341b191c8ffa558da0a721aa639fa5a9
monicabocelli <monica.bocelli93@gmail.com>
2017-01-15 15:55:27 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var magnitude;
     
 
 function setup(){
-    createCanvas(500, 800);
+    createCanvas(400, 500);
    /* 
     //create objects
     for (var i = 0; i < value*5; i++){
@@ -17,9 +17,32 @@ function setup(){
 function draw(){
     background(204);
 
-    magnitude = map(value, 0, 1000, 0, 10);    
-  
+    magnitude = map(value, 0, 1000, 0, 10);
     
+    if (value = 0){
+     textFont('Inconsolata');
+     textSize(30);
+     textAlign(CENTER);
+     textStyle(NORMAL);
+     text("SHAKE YOUR DEVICE", width/2, height/2);    
+    } else if (value > 0){
+        //magnitude indication
+    textFont('Inconsolata');
+    textSize(12);
+    textAlign(CENTER);
+    textStyle(NORMAL);
+    text("Magnitude", width - width/6, height/3.5);
+    textSize(15);
+    textAlign(CENTER);
+    textStyle(BOLD); 
+    
+    text(magnitude, width - width/6, height/2); 
+    
+    textSize(20);
+    text('SEE RESULTS', width/2, height/1.1);
+
+    }    
+ 
         //CREATE THE ELLIPSE AREA
     var x = width/2;
     var y = height/2;
@@ -49,32 +72,10 @@ function draw(){
 
 function deviceShaken(){
     
-    value = value + (accelerationX * accelerationY * accelerationZ);
+    value = accelerationX * accelerationY * accelerationZ;
     
     //QuakeDots();    
-    if (value = 0){
-     textFont('Inconsolata');
-     textSize(30);
-     textAlign(CENTER);
-     textStyle(NORMAL);
-     text("SHAKE YOUR DEVICE", width/2, height/2);    
-    } else if (value > 0){
-        //magnitude indication
-    textFont('Inconsolata');
-    textSize(12);
-    textAlign(CENTER);
-    textStyle(NORMAL);
-    text("Magnitude", width - width/6, height/3.5);
-    textSize(15);
-    textAlign(CENTER);
-    textStyle(BOLD); 
     
-    text(magnitude, width - width/6, height/2); 
-    
-    textSize(20);
-    text('SEE RESULTS', width/2, height/1.1);
-
-    }
     
 }
 

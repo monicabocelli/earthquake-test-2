35cd44bd99a3362970dc8a443e252967d7aa8b96
monicabocelli <monica.bocelli93@gmail.com>
2017-01-17 17:16:25 +0100

    changed the magnitude map to 0,100

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function draw(){
      noStroke();
      text("SHAKE YOUR DEVICE", width/2,height - height/1.1);    
     
-   var magnitude = int(map(value, 0, 10000, 0, 10)); // messo in setup il valore di magnitude è fisso e si vedono i pallini
+   var magnitude = int(map(value, 0, 100, 0, 10)); // messo in setup il valore di magnitude è fisso e si vedono i pallini
     
     if (value > 0){    
         

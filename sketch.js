0fc16da079769200b1cd73f2b9cf9f97c4b6a787
monicabocelli <monica.bocelli93@gmail.com>
2017-01-17 09:55:05 +0100

    modifica in base a l'effetto earthquake

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ var value = 0;  //starting value of earthquake
     
 
 function setup(){
-    createCanvas(400, 600); 
+     createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-    background(204);
-    
+     background(204);
+     angleMode(DEGREES);
     
      textSize(20);
      textAlign(CENTER);
@@ -18,14 +18,14 @@ function draw(){
      noStroke();
      text("SHAKE YOUR DEVICE", width/2, 30);    
     
-    var magnitude = int(map(value, 0, 1000, 0, 10));
+    var magnitude = int(map(value, 0, value, 0, 10));
     
     if (value > 0){
         
         //CREATE THE ELLIPSE AREA
     var x = width/2;
     var y = height/2;
-    var r = value/2; 
+    var r = value; 
     
     noFill();
     stroke(0);
@@ -36,18 +36,22 @@ function draw(){
     fill(0);
     noStroke();    
     
-    textSize(12);
+    textSize(height/40);
     textAlign(CENTER);
     textStyle(NORMAL);
-    text("Magnitude", width - width/7, 300);
-    textSize(20);
+    text("Magnitude", width/2, height - height/5);
+        
+    textSize(height/20);
     textAlign(CENTER);
-    text(magnitude, width - width/7, 340);
-    text(value, width - width/7, 400);
+    textStyle(BOLD);
+    text(magnitude,width/2, height - height/6.7);
+    
+    textSize(height/35);
+    text(value, width/2, height - height/7.2);
         
     textStyle(BOLD);
-    textSize(20);
-    text('SEE RESULTS', width/2, 570);
+    textSize(height/30);
+    text('SEE RESULTS', width/2, height - height/12);
         
     }    
     
@@ -56,7 +60,7 @@ function draw(){
     //draw dots and given methods (actions)
       noStroke();
       fill(0);
-      for (var i = 0; i < value*8; i++){
+      for (var i = 0; i < value*10; i++){
         dots[i].move();
         dots[i]. display();
         
@@ -67,24 +71,28 @@ function draw(){
 
 function deviceShaken(){
     
-    value = (accelerationX * accelerationY * accelerationZ)*2;   
+    value = accelerationX * accelerationY * accelerationZ;   
    
     //create objects
-    for (var i = 0; i < value*8; i++){
+    for (var i = 0; i < value*10; i++){
         dots.push(new QuakeDots());
-    }   
+    } 
     
 }
 
 
 function QuakeDots(){
-    
-    var d = dist(width/2,height/2,width/2+value/2,height/2);
-    
+  
+    var a = random(0,360);
+    var b = random(0,value);//r = value = 30
+    var x = sin(a) * b; // mi dà un numero che va da -b a b
+    var y = cos(a) * b; // mi dà un numero che va da -b a b
+    var d = dist(width/2,height/2, width/2, height/2 + x/2);
     this.xdot = random(width/2 - d, width/2 + d); //according to ellipse area
     this.ydot = random(height/2 - d, height/2 + d); //according to ellipse area
     this.diameter = 4;
     this.speed = 1.5; //magnitude
+    
     this.move = function(){
       this.xdot += random(-this.speed,this.speed);
       this.ydot += random(-this.speed,this.speed);
